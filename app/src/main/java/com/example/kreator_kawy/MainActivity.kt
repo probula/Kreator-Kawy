82e6c7c8c7bc598e7469d5a9f798e3388cba7356
igor <igor.kowalczyk07@poczta.fm>
2025-03-08 22:06:07 +0100

    images are changing after clicking radiobuttons

--- app/src/main/java/com/example/kreator_kawy/MainActivity.kt
+++ app/src/main/java/com/example/kreator_kawy/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kreator_kawy
 
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
+import android.widget.ImageView
+import android.widget.RadioButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val image: ImageView = findViewById(R.id.imageCoffee)
+        val imgEspresso: RadioButton = findViewById(R.id.rbEspresso)
+        val imgCappuccino: RadioButton = findViewById(R.id.rbCappuccino)
+        val imgLatte: RadioButton = findViewById(R.id.rbLatte)
+
+        imgEspresso.setOnClickListener{
+            image.setImageResource(R.drawable.espressozdj)
+        }
+        imgCappuccino.setOnClickListener{
+            image.setImageResource(R.drawable.cappuccinozdj)
+        }
+        imgLatte.setOnClickListener{
+            image.setImageResource(R.drawable.lattezdj)
         }
     }
 }
